a6f7e83951e3ae6cf88cb35ba4ea3ce6b83e30ff
TA2k <tombox2020@gmail.com>
2021-09-08 13:04:33 +0200

    improve error handling

--- main.js
+++ main.js
@@ -231,9 +231,9 @@ class Mystrom extends utils.Adapter {
                     native: {},
                 })
                     .then(() => {
-                        this.setState(path + "." + key, element[key], true).catch((error) => {
+                        this.setState(path + "." + key.replace("!", ""), element[key], true).catch((error) => {
                             this.log.error(error);
-                            this.log.error(path + "." + key + ": " + element[key]);
+                            this.log.error(path + "." + key + ": ");
                         });
                     })
                     .catch((error) => {
