5c82d5d501ae7f423ca7332e3ac42c6b194e9a58
TA2k <tombox2020@gmail.com>
2024-04-21 20:18:39 +0200

    fix test

--- admin/words.js
+++ admin/words.js
@@ -1,41 +1,42 @@
+// eslint-disable-next-line no-unused-vars
 /*global systemDictionary:true */
-"use strict";
+'use strict';
 
 systemDictionary = {
-    "mystrom adapter settings": {
-        "en": "Adapter settings for mystrom",
-        "de": "Adaptereinstellungen für mystrom",
-        "ru": "Настройки адаптера для mystrom",
-        "pt": "Configurações do adaptador para mystrom",
-        "nl": "Adapterinstellingen voor mystrom",
-        "fr": "Paramètres d'adaptateur pour mystrom",
-        "it": "Impostazioni dell'adattatore per mystrom",
-        "es": "Ajustes del adaptador para mystrom",
-        "pl": "Ustawienia adaptera dla mystrom",
-        "zh-cn": "mystrom的适配器设置"
-    },
-    "option1": {
-        "en": "option1",
-        "de": "Option 1",
-        "ru": "Опция 1",
-        "pt": "Opção 1",
-        "nl": "Optie 1",
-        "fr": "Option 1",
-        "it": "opzione 1",
-        "es": "Opción 1",
-        "pl": "opcja 1",
-        "zh-cn": "选项1"
-    },
-    "option2": {
-        "en": "option2",
-        "de": "Option 2",
-        "ru": "option2",
-        "pt": "opção 2",
-        "nl": "Optie 2",
-        "fr": "Option 2",
-        "it": "opzione 2",
-        "es": "opcion 2",
-        "pl": "Opcja 2",
-        "zh-cn": "选项2"
-    }
+  'mystrom adapter settings': {
+    en: 'Adapter settings for mystrom',
+    de: 'Adaptereinstellungen für mystrom',
+    ru: 'Настройки адаптера для mystrom',
+    pt: 'Configurações do adaptador para mystrom',
+    nl: 'Adapterinstellingen voor mystrom',
+    fr: "Paramètres d'adaptateur pour mystrom",
+    it: "Impostazioni dell'adattatore per mystrom",
+    es: 'Ajustes del adaptador para mystrom',
+    pl: 'Ustawienia adaptera dla mystrom',
+    'zh-cn': 'mystrom的适配器设置',
+  },
+  option1: {
+    en: 'option1',
+    de: 'Option 1',
+    ru: 'Опция 1',
+    pt: 'Opção 1',
+    nl: 'Optie 1',
+    fr: 'Option 1',
+    it: 'opzione 1',
+    es: 'Opción 1',
+    pl: 'opcja 1',
+    'zh-cn': '选项1',
+  },
+  option2: {
+    en: 'option2',
+    de: 'Option 2',
+    ru: 'option2',
+    pt: 'opção 2',
+    nl: 'Optie 2',
+    fr: 'Option 2',
+    it: 'opzione 2',
+    es: 'opcion 2',
+    pl: 'Opcja 2',
+    'zh-cn': '选项2',
+  },
 };
